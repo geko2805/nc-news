bb0365a3cb9c69deb903e5f8e8bd957a6748b2fb
geko2805 <gethin.jones28@gmail.com>
2025-03-20 13:51:32 +0000

    add mobile button and links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,26 @@
+import { Link } from "react-router-dom";
 import Articles from "./Articles";
 import Topics from "./Topics";
+import { Button } from "@mui/joy";
 
 function Home() {
   return (
     <>
       <h1>NC NEWS</h1>
-      <Topics />
+      <Link to="/articles" className="all-items">
+        <Button
+          variant="solid"
+          sx={{
+            "--Input-radius": "20px",
+          }}
+        >
+          All articles
+        </Button>
+      </Link>
+      <Topics />{" "}
+      <Link to="/topics" style={{ display: "inline-block" }}>
+        <Button variant="solid">More this way</Button>
+      </Link>
       <Articles />
     </>
   );
